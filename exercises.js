d9b815e7fdf3acc310c9e70c7de0f859ccdcbd92
Keegan Odell <Keegan.Odell@icloud.com>
2023-07-12 19:20:16 -0500

    solved fizzbuzz

--- exercises.js
+++ exercises.js
@@ -1,10 +1,30 @@
 //Write a loop that makes seven calls to console.log to output a triangle
 function looping_triangle() {
-  let pound_tracker = '#'
-  for(let i = 0; i < 7; i++) {
-    console.log(pound_tracker);
-    pound_tracker += '#'
-  }
+	let pound_tracker = '#';
+	for (let i = 0; i < 7; i++) {
+		console.log(pound_tracker);
+		pound_tracker += '#';
+	}
+}
+// looping_triangle()
+
+//FizzBuzz - classic problem about fizzing the buzz.
+//1. if divisible by 3 - fizz
+//2. if divisible by 5 - buzz
+//3. if divisible by both 3 and 5 -FizzBuzz
+// note: question asking for numbers 1-100
+function fizz_buzz() {
+	for (let i = 1; i <= 100; i++) {
+		if(i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else {
+      console.log(i);
+    }
+	}
 }
 
-// looping_triangle()
+fizz_buzz()
